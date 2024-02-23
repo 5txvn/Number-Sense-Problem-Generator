050b0015e5dc5b548d3e4871268c971f323d4ba2
Steven <61475919+5txvn@users.noreply.github.com>
2024-02-23 15:00:42 -0600

    commit for the day

--- js/practice.js
+++ js/practice.js
@@ -1,8 +1,4 @@
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
 const parameters = code.split("-");
-console.log(parameters[0])
-console.log(parseInt(parameters[1]))
-console.log(parseInt(parameters[2]))
-console.log(parseInt(parameters[3]))
 $(".problem").text(generateProblem(parameters[0], parseInt(parameters[1]), parseInt(parameters[2]), parseInt(parameters[3])))
--- js/problem-generation.js
+++ js/problem-generation.js
@@ -17,13 +17,16 @@ function genMult(factor, digits) {
 function genFOIL(digits, digits2) {
     digits = parseInt(digits);
     digits2 = parseInt(digits2);
-    return `$${generateNum(10**(digits-1), 10**(digits))} \\times ${generateNum(10**(digits-1), 10**(digits))}$`
+    const num1 = generateNum(10**(digits-1), 10**(digits));
+    const num2 = generateNum(10**(digits-1), 10**(digits));
+    return [`$${num1} \\times ${num2}$`, num1 * num2]
 }
 //problem generation function
 function generateProblem(code, digits, digits2, factor) {
     //check for the codes and return the appropriate problem in latex
     switch(code) {
         case "mult":
+            $(".problem").html(generateProblem(""))
             return genMult(factor, digits)
             break;
         case "foil":
